605800c13c90de33012ffe5c96cbff9394f11c0e
Gabriellamm <379713491@qq.com>
2017-12-31 08:50:19 +0800

    push

---
+++ Best/Function/FunctViewController.h
@@ -0,0 +1,13 @@
+//
+//  FunctViewController.h
+//  Best
+//
+//  Created by Gabriella on 2017/12/28.
+//  Copyright © 2017年 Gabriella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FunctViewController : UIViewController
+
+@end
---
+++ Best/Function/FunctViewController.m
@@ -0,0 +1,48 @@
+//
+//  FunctViewController.m
+//  Best
+//
+//  Created by Gabriella on 2017/12/28.
+//  Copyright © 2017年 Gabriella. All rights reserved.
+//
+
+#import "FunctViewController.h"
+
+@interface FunctViewController ()
+
+@end
+
+@implementation FunctViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+
+}
+
+    -(void)iamgeStrehable{
+
+        UIImage *image =[UIImage imageNamed:@""];
+//        UIImage *stretchImage = [image stretchableImageWithLeftCapWidth:<#(NSInteger)#> topCapHeight:<#(NSInteger)#>];
+        UITextField *teF = [UITextField new];
+//        teF.contentVerticalAlignment
+        teF.enablesReturnKeyAutomatically  =YES;// 输入框有值才能点击键盘
+self.navigationController setNavigationBarHidden:<#(BOOL)#> animated:<#(BOOL)#>
+    }
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
